45d4589ad3e63326b15f0537673027fd5f506e9c
Jack DesCombes <jpdesc@alumni.stanford.edu>
2022-11-19 17:56:59 -0800

    Configure cookies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,11 +8,24 @@ import { ChannelListContainer, ChannelContainer, Auth } from './components';
 import 'stream-chat-react/dist/css/index.css';
 import './App.css';
 
+const cookies = new Cookies();
+
 const apiKey = 'u4877raa7y84';
 
 const client = StreamChat.getInstance(apiKey);
 
-const authToken = false;
+const authToken = cookies.get('token');
+
+if (authToken) {
+    client.connectUser({
+        name: cookies.get('username'),
+        fullName: cookies.get('fullName'),
+        id: cookies.get('userId'),
+        phoneNumber: cookies.get('phoneNumber'),
+        image: cookies.get('avatarURL'),
+        hashedPassword: cookies.get('hashedPassword'),
+    }, authToken)
+}
 
 
 const App = () => {
