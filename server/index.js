73e3987a576cd58ce76245b8b93ffffac77c7592
Jack DesCombes <jpdesc@alumni.stanford.edu>
2022-12-07 14:26:19 -0800

    * Update url

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
 const twilioClient = require('twilio')(accountSid, authToken);
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
